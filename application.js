afc668ac540dfdfa9f4f8bf4fd101141674ccdb4
Dominika <dominikazajac00@gmail.com>
2018-02-24 16:49:46 +0100

    add js logic

--- application.js
+++ application.js
@@ -1,5 +1,176 @@
-var pages;
+var levels = [500, 1000, 2000, 5000, 10000, 20000, 40000, 75000, 125000, 250000, 500000, 1000000];
+var currLevel;
+var points;
+var usedQuestions;
+var numberOfQuestions;
+var isLost;
 
-$(document).ready(function() {
-    pages = UIkit.switcher('#pages');
-});
+(function () {
+    numberOfQuestions = questions.length;
+    if (numberOfQuestions < 12) {
+        alert("You need at least 12 questions!");
+    }
+})();
+
+function loadHomeScreen() {
+    $('.intro-section').show();
+    $('.question-container').hide();
+    $(".next-button").text("Nastepne pytanie");
+    $(".level-" + currLevel).removeClass("uk-label-warning");
+    for (var i = 0; i < currLevel + 1; i++) {
+        $(".level-" + i).removeClass("uk-label-success");
+    }
+    $(".level-0").addClass("uk-label-warning");
+    setHelpers('disabled');
+}
+
+function startGame() {
+    points = 0;
+    currLevel = 0;
+    isLost = false;
+    usedQuestions = [];
+    $('.intro-section').hide();
+    $('.question-container').show();
+    loadQuestion();
+    setHelpers('start');
+}
+
+function endGame() {
+    $("#end-game span").text(countScore());
+    UIkit.modal("#end-game").show();
+}
+
+function checkAnswer(event) {
+    $(event).css("border", "5px solid gold");
+    var isCorrect = $(event).attr("data-correct");
+    if (isCorrect === 'false') {
+        isLost = true;
+        $(".next-button").text("Zobacz wygraną");
+        $(event).css("background-color", '#d32c46');
+        for (var i = 0; i < 4; i++) {
+            var isCorrect = $(".answer-" + i).attr("data-correct");
+            if (isCorrect !== 'false') {
+                $(".answer-" + i).css("background-color", '#32d296');
+            }
+            $(".answer-" + i).attr("disabled", true);
+        }
+    } else {
+        $(event).css("background-color", '#32d296');
+        $(".level-" + currLevel).removeClass("uk-label-warning");
+        $(".level-" + currLevel).addClass("uk-label-success");
+        for (var i = 0; i < 4; i++) {
+            $(".answer-" + i).attr("disabled", true);
+        }
+        currLevel++;
+    }
+    $(".next-button").show();
+}
+
+function nextQuestion() {
+    if (isLost) {
+        endGame();
+    } else if (currLevel < 12) {
+        loadQuestion();
+    } else {
+        UIkit.modal("#won-milion").show();
+    }
+    $(".next-button").hide();
+}
+
+function loadQuestion() {
+    $(".level-" + currLevel).addClass("uk-label-warning");
+    var index = chooseQuestion();
+    var question = questions[index];
+    $(".question-text > h2").text(question.text);
+    var correctAnswer = question.answers[0];
+    var answers = question.answers;
+    shuffle(answers);
+
+    for (var i = 0; i < 4; i++) {
+        var answer = question.answers[i];
+        var isCorrect = (correctAnswer === answer);
+        var letter = String.fromCharCode(i + 65) + ") ";
+        $('.answer-' + i).text(letter + answer);
+        $('.answer-' + i).attr("data-correct", isCorrect);
+        $('.answer-' + i).css("background-color", "");
+        $('.answer-' + i).css("border", "");
+        $('.answer-' + i).attr("disabled", false);
+    }
+}
+
+function countScore() {
+    if (currLevel < 2) return 0;
+    else if (currLevel < 6) return levels[1];
+    else if (currLevel < 12) return levels[6];
+    else return levels[11];
+}
+
+function chooseQuestion() {
+    var index = Math.floor(Math.random() * numberOfQuestions);
+    while (usedQuestions.indexOf(index) !== -1) {
+        index = Math.floor(Math.random() * numberOfQuestions);
+    }
+    usedQuestions.push(index);
+    return index;
+}
+
+function shuffle(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var x = array[i];
+        array[i] = array[j];
+        array[j] = x;
+    }
+}
+
+function fiftyFifty() {
+    $("#helper-fifty").addClass("custom-disabled");
+    var choosen = [];
+    while (choosen.length < 2) {
+        var number = Math.floor(Math.random() * 4);
+        if (choosen.indexOf(number) < 0) {
+            var isCorrect = $('.answer-' + number).attr("data-correct");
+            if (isCorrect !== 'true') {
+                $('.answer-' + number).attr("disabled", true);
+                choosen.push(number);
+            }
+        }
+    }
+}
+
+function askAudience() {
+    $("#helper-audience").addClass("custom-disabled");
+    countdown(120);
+}
+
+function callFriend() {
+    $("#helper-phone").addClass("custom-disabled");
+    countdown(60);
+}
+
+function countdown(time) {
+    $("#timeleft").text(time);
+    $("#timer").show();
+    var downloadTimer = setInterval(function () {
+        time--;
+        $("#timeleft").text(time);
+        if (time <= 0) {
+            clearInterval(downloadTimer);
+            UIkit.modal("#end-of-time").show();
+            $('#timer').hide();
+        }
+    }, 1000);
+}
+
+function setHelpers(val){
+    if (val === 'disabled') {
+        $("#helper-audience").addClass("custom-disabled");
+        $("#helper-fifty").addClass("custom-disabled");
+        $("#helper-phone").addClass("custom-disabled");
+    }else {
+        $("#helper-audience").removeClass("custom-disabled");
+        $("#helper-fifty").removeClass("custom-disabled");
+        $("#helper-phone").removeClass("custom-disabled");
+    }
+
+}
--- questions.js
+++ questions.js
@@ -1,137 +1,139 @@
-﻿var pytania = [
-    //uwaga: pierwsza odpowiedź jest prawidłowa, randomizcja w czasie runtime!
+﻿var questions = [
     {
-        pytanie: "Ile bajtów ma kilobajt?", odpowiedzi: ["1024", "1000", "miliard", "8"]
+        text: "Ile bajtów ma kilobajt?",
+        answers: ["1024", "1000", "miliard", "8"]
     },
     {
-        pytanie: "Ile bajtów ma megabajt?", odpowiedzi: ["1048576", "miliard", "1024", "1000"]
+        text: "Ile bajtów ma megabajt?",
+        answers: ["1048576", "miliard", "1024", "1000"]
     },
     {
-        pytanie: "Ile bajtów ma bit?", odpowiedzi: ["1/8", "8", "1024", "128"]
+        text: "Ile bajtów ma bit?",
+        answers: ["1/8", "8", "1024", "128"]
     },
     {
-        pytanie: "Co to jest ZIP?",
-        odpowiedzi: ["format kompresji", "format obrazka", "format dokumentu", "plik wykonywalny"]
+        text: "Co to jest ZIP?",
+        answers: ["format kompresji", "format obrazka", "format dokumentu", "plik wykonywalny"]
     },
-    //{pytanie: "Ile lat ma KSI?", odpowiedzi: ["41","16","100","40"]},
+    //{text: "Ile lat ma KSI?", answers: ["41","16","100","40"]},
     {
-        pytanie: "Jakim skrótem klawiaturowym możesz cofnąć ostatnią zmianę?",
-        odpowiedzi: ["Ctrl+Z", "Ctrl+Y", "Ctrl+B", "Ctrl+Alt+Del"]
-    },
-    {
-        pytanie: "Jakim poleceniem można wydrukować określony komunikat na ekran w konsoli linuksowej?",
-        odpowiedzi: ["echo", "ls", "dir", "send"]
+        text: "Jakim skrótem klawiaturowym możesz cofnąć ostatnią zmianę?",
+        answers: ["Ctrl+Z", "Ctrl+Y", "Ctrl+B", "Ctrl+Alt+Del"]
     },
     {
-        pytanie: "Kiedy odbywa się 13. edycja Studenckiego Festiwalu Informatycznego?",
-        odpowiedzi: ["16-18.03", "15-20.04", "8-10.04", "1-3.03"]
+        text: "Jakim poleceniem można wydrukować określony komunikat na ekran w konsoli linuksowej?",
+        answers: ["echo", "ls", "dir", "send"]
     },
     {
-        pytanie: "Które z poniższych NIE jest przeglądarką?", odpowiedzi: ["Word", "Chrome", "Frefox", "Safari"]
+        text: "Kiedy odbywa się 13. edycja Studenckiego Festiwalu Informatycznego?",
+        answers: ["16-18.03", "15-20.04", "8-10.04", "1-3.03"]
     },
     {
-        pytanie: "Kto jest autorem jądra systemu operacyjnego Linux?",
-        odpowiedzi: ["Linus Torvalds", "Richard M. Stallman", "Anthony Linux", "Bill Gates"]
+        text: "Które z poniższych NIE jest przeglądarką?", answers: ["Word", "Chrome", "Frefox", "Safari"]
     },
     {
-        pytanie: "Który z poniższych nie jest systemem operacyjnym?", odpowiedzi: ["Arduino", "FreeBSD", "OS X", "DOS"]
+        text: "Kto jest autorem jądra systemu operacyjnego Linux?",
+        answers: ["Linus Torvalds", "Richard M. Stallman", "Anthony Linux", "Bill Gates"]
     },
     {
-        pytanie: "Jaki test musiałaby zdać maszyna, aby można było uznać, że myśli?",
-        odpowiedzi: ["Turinga", "McDonalda", "Assena", "Ahera"]
+        text: "Który z poniższych nie jest systemem operacyjnym?", answers: ["Arduino", "FreeBSD", "OS X", "DOS"]
     },
     {
-        pytanie: "Do czego służy CAPTCHA?",
-        odpowiedzi: ["do potwierdzenia że informację wysyła człowiek", "do szyfrowania wiadomości", "do tworzenia testów automatycznych", "do przeprowadzania wyborów internetowych"]
+        text: "Jaki test musiałaby zdać maszyna, aby można było uznać, że myśli?",
+        answers: ["Turinga", "McDonalda", "Assena", "Ahera"]
     },
     {
-        pytanie: "Który komputer jest najstarszy?", odpowiedzi: ["Eniac", "Amiga", "Odra", "Macintosh"]
+        text: "Do czego służy CAPTCHA?",
+        answers: ["do potwierdzenia że informację wysyła człowiek", "do szyfrowania wiadomości", "do tworzenia testów automatycznych", "do przeprowadzania wyborów internetowych"]
     },
     {
-        pytanie: "Który z poniższych programów NIE służy do tworzenia grafiki?",
-        odpowiedzi: ["Wolfram Alpha", "MS Paint", "GIMP", "Photoshop"]
-    },
-    //{pytanie: "<span class='s'?>Jak nazywamy łamanie zabezpieczeń sieci telefonicznych, najczęściej celem uzyskania połączenia darmowego lub tańszego niż tradycyjne?</span>", odpowiedzi: ["phreaking","hacking","threating","handling"]},
-    {
-        pytanie: "Pod jakim adresem mieści się Wydział Matematyki i Informatyki UJ?",
-        odpowiedzi: ["Łojasiewicza 6", "Norymberska 12", "Ruczajowa 5", "Gołębia 23"]
+        text: "Który komputer jest najstarszy?", answers: ["Eniac", "Amiga", "Odra", "Macintosh"]
     },
     {
-        pytanie: "Który adres IP jest nieprawidłowy?",
-        odpowiedzi: ["192.268.1.201", "127.0.0.1", "::1", "149.156.65.204"]
+        text: "Który z poniższych programów NIE służy do tworzenia grafiki?",
+        answers: ["Wolfram Alpha", "MS Paint", "GIMP", "Photoshop"]
+    },
+    //{text: "<span class='s'?>Jak nazywamy łamanie zabezpieczeń sieci telefonicznych, najczęściej celem uzyskania połączenia darmowego lub tańszego niż tradycyjne?</span>", answers: ["phreaking","hacking","threating","handling"]},
+    {
+        text: "Pod jakim adresem mieści się Wydział Matematyki i Informatyki UJ?",
+        answers: ["Łojasiewicza 6", "Norymberska 12", "Ruczajowa 5", "Gołębia 23"]
     },
     {
-        pytanie: "Jak nazywani są hakerzy działający zupełnie legalnie lub też starający się nie wyrządzać szkód?",
-        odpowiedzi: ["whitehat", "redhat", "blackhat", "Bonzo"]
+        text: "Który adres IP jest nieprawidłowy?",
+        answers: ["192.268.1.201", "127.0.0.1", "::1", "149.156.65.204"]
     },
     {
-        pytanie: "Który kabel służy do połączeń sieciowych?", odpowiedzi: ["skrętka", "molex", "SATA", "DVI"]
+        text: "Jak nazywani są hakerzy działający zupełnie legalnie lub też starający się nie wyrządzać szkód?",
+        answers: ["whitehat", "redhat", "blackhat", "Bonzo"]
     },
     {
-        pytanie: "Które z poniższych NIE służy do połączania dysków?", odpowiedzi: ["MOLEX", "iSCSI", "SATA", "USB"]
+        text: "Który kabel służy do połączeń sieciowych?", answers: ["skrętka", "molex", "SATA", "DVI"]
     },
     {
-        pytanie: "Jak nazwyał się pierwszy polski Internet?", odpowiedzi: ["NASK", "Odra", "CERT", "DNS"]
+        text: "Które z poniższych NIE służy do połączania dysków?", answers: ["MOLEX", "iSCSI", "SATA", "USB"]
     },
     {
-        pytanie: "Co odpowiada za zamianę nazw domenowych (np. google.pl) na adresy IP",
-        odpowiedzi: ["DNS", "DHCP", "ARP", "TCP/IP"]
-    },
-    //{pytanie: "Co robi klawisz SysRq w Linuksie?", odpowiedzi: ["zapewnia dostęp do debugera jądra","nic - był wykozrsytywany tylko w DOSie","przyspiesza procesor","służy do blokowania ekranu"]},
-    //{pytanie: "12 Ile bajtów ma bit?", odpowiedzi: ["1/8","8","1024","128"]},//pytań nie może być mniej jak 12!!!
-    {
-        pytanie: "Ile porównań potrzeba, aby posortować ciąg 1000 elementów? Wybierz najmniejszą wystarczającą liczbę",
-        odpowiedzi: ["10000", "10", "1000", "100000"]
+        text: "Jak nazywał się pierwszy polski Internet?", answers: ["NASK", "Odra", "CERT", "DNS"]
     },
     {
-        pytanie: "Jaka jest najpopularniejsza architektura procesorów komputerowych?",
-        odpowiedzi: ["x86", "sparc", "powerpc", "avr"]
+        text: "Co odpowiada za zamianę nazw domenowych (np. google.pl) na adresy IP",
+        answers: ["DNS", "DHCP", "ARP", "TCP/IP"]
+    },
+    //{text: "Co robi klawisz SysRq w Linuksie?", answers: ["zapewnia dostęp do debugera jądra","nic - był wykozrsytywany tylko w DOSie","przyspiesza procesor","służy do blokowania ekranu"]},
+    //{text: "12 Ile bajtów ma bit?", answers: ["1/8","8","1024","128"]},//pytań nie może być mniej jak 12!!!
+    {
+        text: "Ile porównań potrzeba, aby posortować ciąg 1000 elementów? Wybierz najmniejszą wystarczającą liczbę",
+        answers: ["10000", "10", "1000", "100000"]
     },
     {
-        pytanie: "Kto jest twórcą najpopularniejszej architektury komputerowej?",
-        odpowiedzi: ["John von Neumann", "Alan Turing", "Albert Einstein", "Linus Torvalds"]
+        text: "Jaka jest najpopularniejsza architektura procesorów komputerowych?",
+        answers: ["x86", "sparc", "powerpc", "avr"]
     },
     {
-        pytanie: "Ile okładek książek wystarczy przeczytać, aby znaleźć określoną pozycję na półce 1000 ułożonych alfabetycznie książek?",
-        odpowiedzi: ["10", "5", "50", "100"]
+        text: "Kto jest twórcą najpopularniejszej architektury komputerowej?",
+        answers: ["John von Neumann", "Alan Turing", "Albert Einstein", "Linus Torvalds"]
     },
     {
-        pytanie: "Co to jest Maszyna Turinga?",
-        odpowiedzi: ["Teroretyczny model obliczeń", "Maszyna rozszyfrowująca kod enigmy", "komputer zaprojektowany przez Alana Turinga", "algorytm wyszukiwania słów w tekście"]
+        text: "Ile okładek książek wystarczy przeczytać, aby znaleźć określoną pozycję na półce 1000 ułożonych alfabetycznie książek?",
+        answers: ["10", "5", "50", "100"]
     },
     {
-        pytanie: "Co to jest SQL?",
-        odpowiedzi: ["Język zapytań do baz danych", "System do przechowywania informacji", "Język do tworzenia stron internetowych", "Program do tworzenia raportów finansowych"]
+        text: "Co to jest Maszyna Turinga?",
+        answers: ["Teroretyczny model obliczeń", "Maszyna rozszyfrowująca kod enigmy", "komputer zaprojektowany przez Alana Turinga", "algorytm wyszukiwania słów w tekście"]
     },
     {
-        pytanie: "Który z poniższych nie jest językiem programowania?", odpowiedzi: ["XML", "C++", "Java", "Haskel"]
+        text: "Co to jest SQL?",
+        answers: ["Język zapytań do baz danych", "System do przechowywania informacji", "Język do tworzenia stron internetowych", "Program do tworzenia raportów finansowych"]
     },
     {
-        pytanie: "Który z poniższych systemów operacyjnych powstał jako powierszy?",
-        odpowiedzi: ["Unix", "Windows 95", "Linux", "MsDOS"]
+        text: "Który z poniższych nie jest językiem programowania?", answers: ["XML", "C++", "Java", "Haskel"]
     },
     {
-        pytanie: "Jaka jest pojemność płyty CD?", odpowiedzi: ["700MB", "4,7GB", "50GB", "500kB"]
+        text: "Który z poniższych systemów operacyjnych powstał jako pierwszy?",
+        answers: ["Unix", "Windows 95", "Linux", "MsDOS"]
     },
     {
-        pytanie: "Którym z kabli nie podłączymy monitora?", odpowiedzi: ["SAS", "HDMI", "DisplayPort", "VGA"]
+        text: "Jaka jest pojemność płyty CD?", answers: ["700MB", "4,7GB", "50GB", "500kB"]
     },
     {
-        pytanie: "Które z poniższych słów jest nazwą języka programowania?",
-        odpowiedzi: ["Python", "Anaconda", "Cobra", "Boa"]
+        text: "Którym z kabli nie podłączymy monitora?", answers: ["SAS", "HDMI", "DisplayPort", "VGA"]
     },
     {
-        pytanie: "Liczba 81 w systemie szesnastkowym to", odpowiedzi: ["51", "1010001", "121", "3E"]
+        text: "Które z poniższych słów jest nazwą języka programowania?",
+        answers: ["Python", "Anaconda", "Cobra", "Boa"]
     },
     {
-        pytanie: "Które z protokołów służy do wysyłania poczty email?", odpowiedzi: ["smtp", "pop3", "ftp", "imap"]
+        text: "Liczba 81 w systemie szesnastkowym to", answers: ["51", "1010001", "121", "3E"]
     },
     {
-        pytanie: "Czym w informatyce jest demon (daemon)?",
-        odpowiedzi: ["proces pracujacy w tle", "rodzaj wirusa komputerowego", "błędny program zachowywujący się jak nawiedzony", "typ protokołu sieciowego"]
+        text: "Które z protokołów służy do wysyłania poczty email?", answers: ["smtp", "pop3", "ftp", "imap"]
     },
     {
-        pytanie: "Używanie której technologii NIE poprawi naszego bezpieczeństwa w internecie?",
-        odpowiedzi: ["HTTP", "VPN", "TLS", "SFTP"]
+        text: "Czym w informatyce jest demon (daemon)?",
+        answers: ["proces pracujacy w tle", "rodzaj wirusa komputerowego", "błędny program zachowywujący się jak nawiedzony", "typ protokołu sieciowego"]
+    },
+    {
+        text: "Używanie której technologii NIE poprawi naszego bezpieczeństwa w internecie?",
+        answers: ["HTTP", "VPN", "TLS", "SFTP"]
     }
 ];
